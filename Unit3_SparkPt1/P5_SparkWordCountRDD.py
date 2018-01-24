0a4916dbc4b8999952f64d56b4b5995a4c993340
katiehouse3 <katiehouse3@gmail.com>
2018-01-24 16:47:48 -0500

    delete Unit 1 Spark

--- Unit3_SparkPt1/P5_SparkWordCountRDD.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkConf, SparkContext
-
-# Connect to spark session and to Ulysses doc
-conf = SparkConf().setMaster("local").setAppName("MyApp")
-sc = SparkContext(conf = conf)
-
-# Read Ulysses doc
-lines = sc.textFile("ulysses10.txt")
-print lines.count()
